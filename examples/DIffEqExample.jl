049bdb65be6969fa96e52c5d2155fc1ee03b3f3d
kazewong <kazewong.physics@gmail.com>
2024-06-08 19:20:44 -0400

    feat: Add waveform equation example using CUDA

---
+++ examples/DIffEqExample.jl
@@ -0,0 +1,25 @@
+using DifferentialEquations, CUDA
+
+const N = 1000
+const c = 1.0
+const u0 = zeros(Float32, N)
+u0[1] = 1.0
+u0[N] = 1.0
+p = [c]
+
+function wave_equation(du, u, p, t)
+    c = p[1]
+    N = length(u)
+    du[1] = 0.0
+    du[N] = 0.0
+    for i in 2:N-1
+        du[i] = c^2 * (u[i+1] - 2u[i] + u[i-1])
+    end
+end
+
+tspan = (0.0f0, 1.0f0)
+prob = ODEProblem(wave_equation, u0, tspan, p)
+sol = solve(prob, Tsit5(), saveat=0.01f0)
+
+using Plots
+plot(sol, label="")

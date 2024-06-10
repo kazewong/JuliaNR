ae5b2af4f1056cdaffdc7f86e2a9a9c8c6ad3c14
kazewong <kazewong.physics@gmail.com>
2024-06-09 23:33:18 -0400

    Scaffolding

--- src/Systems.jl
+++ src/Systems.jl
@@ -1,15 +1,11 @@
-using ModelingToolkit
+struct field{T}
+    α::T
+    β::AbstractArray{T}
+    Aij::AbstractArray{T}
+    
 
-# @mtkmodel BSSN begin
-#     @parameters t
-#     @variables α β^i γ_ij K_ij R
-#     @derivatives D'~t
+end
 
-#     # Define the BSSN equations
-#     Dα = -2*α*K
-#     Dβ^i = 3/4*α*B^i
-#     Dγ_ij = -2*α*A_ij
-#     DK_ij = α*(R_ij - 2*K*K_ij + 1/2*γ_ij*K^2)
-#     DR = γ_ij*K_ij*K_ij - K^2*R + 2*α*K*R_ij - 1/2*α*γ_ij*R^ij
-# end
+function ChristoffelSymbols(metric::AbstractArray)
 
+end

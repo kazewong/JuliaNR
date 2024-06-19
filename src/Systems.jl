ed89978832635c5dbfab4ebdea33eb5e05694373
kazewong <kazewong.physics@gmail.com>
2024-06-18 23:43:30 -0400

    Implementing BSSN system

--- src/Systems.jl
+++ src/Systems.jl
@@ -1,9 +1,26 @@
 using Trixi
+using Symbolics
+using StaticArrays
+using LinearAlgebra
 
-struct 3DBSSN <: Trixi.AbstractEquations{3, 24}
-    # α::T
-    # β::AbstractArray{T}
-    # Aij::AbstractArray{T}
+struct BSSNSystem{T}
+    # Scalars
+    ϕ::T
+    α::T
+    K::T
+
+    # Vectors
+    β::SVector{3, T}
+    B::SVector{3, T}
+    Γ_tilt::SVector{3, T}
+
+    # Symmetric rank-2 tensor
+    γ_tilt::Symmetric{SMatrix{3, 3, T}}
+    Aij_tilt::Symmetric{SMatrix{3, 3, T}}
+end
+
+function rhs!(dfield::SVector{BSSNSystem, 3}field::BSSNSystem, )
+
 end
 
 Trixi.flux

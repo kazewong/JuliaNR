ffa4e2477cd1d422dd79224014207ea089c45c6f
Kaze Wong <kazewong.physics@gmail.com>
2024-06-20 17:00:21 -0400

    Update PDESystem with additional variables and domains

--- src/systems.jl
+++ src/systems.jl
@@ -21,45 +21,45 @@ using DifferentialEquations
 #     Aij_tilt::Symmetric{SMatrix{3, 3, T}}
 # end
 
-@parameters t x# y z
+@parameters t x y z
 @variables ϕ(..) α(..) K(..)# (β(..))[1:3] (B(..))[1:3] (Γ_tilt(..))[1:3] γ_tilt((..))[1:3, 1:3] (Aij_tilt(..))[1:3, 1:3]
 
 ∂t = Differential(t)
-∂x = Differential(x)#.((x, y, z))
+∂x = Differential.((x, y, z))
 
 eqs = [
-    ∂t(ϕ(t,x)) ~ - α(t,x) * K(t,x),
-    ∂t(α(t,x)) ~ -2 * α(t,x) * K(t,x),
-    ∂t(K(t,x)) ~ -∂x(∂x(ϕ(t,x))),
+    ∂t(ϕ(t,x,y,z)) ~ - α(t,x,y,z) * K(t,x,y,z),
+    ∂t(α(t,x,y,z)) ~ -2 * α(t,x,y,z) * K(t,x,y,z),
+    ∂t(K(t,x,y,z)) ~ -∂x[1](∂x[1](ϕ(t,x,y,z))),
 ]
     # ∂t(ϕ) ~ sum(map(i -> β(t,x,y,z)[i]*∂x[i](ϕ(t,x,y,z)), 1:3)) + (sum(map(i -> ∂x[i](β(t,x,y,z)[i]), 1:3)) - α(t,x,y,z) * K(t,x,y,z)) /6,
     # ,
 
 
 bcs = [
-    ϕ(t, 0) ~ 0.0,
-    ϕ(t, 1) ~ 0.0,
-    # ϕ(t, x, 0, z) ~ 0.0,
-    # ϕ(t, x, 1, z) ~ 0.0,
-    # ϕ(t, x, y, 0) ~ 0.0,
-    # ϕ(t, x, y, 1) ~ 0.0,
-    ϕ(0, x) ~ 0.0,
+    ϕ(t, 0, y, z) ~ 0.0,
+    ϕ(t, 1, y, z) ~ 0.0,
+    ϕ(t, x, 0, z) ~ 0.0,
+    ϕ(t, x, 1, z) ~ 0.0,
+    ϕ(t, x, y, 0) ~ 0.0,
+    ϕ(t, x, y, 1) ~ 0.0,
+    ϕ(0, x, y, z) ~ 0.0,
 
-    α(t, 0) ~ 1.0,
-    α(t, 1) ~ 1.0,
-    # α(t, x, 0, z) ~ 1.0,
-    # α(t, x, 1, z) ~ 1.0,
-    # α(t, x, y, 0) ~ 1.0,
-    # α(t, x, y, 1) ~ 1.0,
-    α(0, x) ~ 1.0,
+    α(t, 0, y, z) ~ 1.0,
+    α(t, 1, y, z) ~ 1.0,
+    α(t, x, 0, z) ~ 1.0,
+    α(t, x, 1, z) ~ 1.0,
+    α(t, x, y, 0) ~ 1.0,
+    α(t, x, y, 1) ~ 1.0,
+    α(0, x, y, z) ~ 1.0,
 
-    K(t, 0) ~ 0.0,
-    K(t, 1) ~ 0.0,
-    # K(t, x, 0, z) ~ 0.0,
-    # K(t, x, 1, z) ~ 0.0,
-    # K(t, x, y, 0) ~ 0.0,
-    # K(t, x, y, 1) ~ 0.0,
-    K(0, x) ~ 0.0,
+    K(t, 0, y, z) ~ 0.0,
+    K(t, 1, y, z) ~ 0.0,
+    K(t, x, 0, z) ~ 0.0,
+    K(t, x, 1, z) ~ 0.0,
+    K(t, x, y, 0) ~ 0.0,
+    K(t, x, y, 1) ~ 0.0,
+    K(0, x, y, z) ~ 0.0,
 
     # β(t, 0, y, z) .~ 0.0,
     # β(t, 1, y, z) .~ 0.0,
@@ -108,14 +108,14 @@ t_max = 10.0
 
 domains = [
     x ∈ Interval(x_min, x_max),
-    # y ∈ Interval(y_min, y_max),
-    # z ∈ Interval(z_min, z_max),
+    y ∈ Interval(y_min, y_max),
+    z ∈ Interval(z_min, z_max),
     t ∈ Interval(t_min, t_max)
 ]
 
 # @named bssn_system = PDESystem(eqs, bcs, domains, [t, x, y, z], [ϕ(t,x,y,z), α(t,x,y,z), K(t,x,y,z), β(t,x,y,z), B(t,x,y,z), Γ_tilt(t,x,y,z), γ_tilt(t,x,y,z), Aij_tilt(t,x,y,z)])
 
-@named bssn_system = PDESystem(eqs, bcs, domains, [t, x], [ϕ(t,x), α(t,x), K(t,x)])
+@named bssn_system = PDESystem(eqs, bcs, domains, [t, x, y, z], [ϕ(t,x, y, z), α(t,x, y, z), K(t,x, y, z)])
 
 N = 32
 dx = (x_max - x_min) / N
@@ -124,7 +124,7 @@ dz = (z_max - z_min) / N
 
 order = 2
 
-discretization = MOLFiniteDifference([x => dx], t, approx_order = order, grid_align = center_align)
+discretization = MOLFiniteDifference([x => dx, y => dy, z => dz], t, approx_order = order, grid_align = center_align)
 
 prob = discretize(bssn_system, discretization)
 sol = solve(prob, TRBDF2(), saveat = 0.1, progress=true)

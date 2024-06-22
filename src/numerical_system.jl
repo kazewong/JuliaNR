9924d1e1aa3a8666ccb9fdc852268ba60a24fce3
kazewong <kazewong.physics@gmail.com>
2024-06-21 22:56:06 -0400

    Refactor α_rhs! function to improve readability and performance

--- src/numerical_system.jl
+++ src/numerical_system.jl
@@ -44,6 +44,10 @@ function christoffel_symbol(γ_inv, ∂γ, i, j, k)
     return 0.5 * sum(map(l -> γ_inv[k, l] * (∂γ[j, l, i] + ∂γ[i, l, j] - ∂γ[i, j, l]), 1:3))
 end
 
+function α_rhs!(update::BSSNVariables, u::BSSNVariables, ∂u::BSSNVariables, ∂2u::BSSNVariables, γ_inv::SMatrix{3, 3, T})
+    update.α = -u.α * u.K + sum([u.β[i]*∂u.K[i] for i in 1:3]) - sum([γ_inv[i][j]*(∂2u.α[i][j] - sum([christoffel_symbol(γ_inv, ∂u.γ, k, i, j)*∂u.α[k] for k in 1:3])) for i in 1:3 for j in 1:3])
+end
+
 function K_rhs!(update::Array{BSSNVariables, 3}, u::BSSNVariables, ∂u::BSSNVariables, ∂2u::BSSNVariables, γ_inv::SMatrix{3, 3, T})
     update.K =  u.α * (sum([γ_inv[i, k] * γ_inv[j, l] * u.Aij_tilt[k, l] * u.Aij_tilt[i, j] for i in 1:3 for j in 1:3 for k in 1:3 for l in 1:3]) + u.K^2/3) + sum([u.β[i]*∂u.K[i] for i in 1:3]) - sum([γ_inv[i][j]*(∂2u.α[i][j] - sum([christoffel_symbol(γ_inv, ∂u.γ, k, i, j)*∂u.α[k] for k in 1:3])) for i in 1:3 for j in 1:3])
 end
@@ -58,7 +62,8 @@ function update_rhs!(update::Array{BSSNVariables, 3}, system::BSSNSystem, p, t)
 
                 γ_inv = inv(variables.γ_tilt)
 
-                update[x₁, x₂, x₃].ϕ = sum(map(i -> u.β[i]*∂u[i].ϕ, 1:3)) + sum(map(i -> system.∂u[x₁, x₂, x₃, i].β[i], 1:3))
+                α_rhs!(update[x₁, x₂, x₃], u, ∂u, ∂2u, γ_inv)
+                # update[x₁, x₂, x₃].ϕ = sum(map(i -> u.β[i]*∂u[i].ϕ, 1:3)) + sum(map(i -> system.∂u[x₁, x₂, x₃, i].β[i], 1:3))
                 K_rhs!(update[x₁, x₂, x₃], u, ∂u, ∂2u, γ_inv)
 
                 for i in 1:3
